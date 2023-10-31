1844b00196e147d874eb92de34d43e3e92cb5eee
Omri Goldberger <omrigoldberger@gmail.com>
2023-10-31 23:31:41 +0200

    dist shouldn't be part of the repo, also combined some of the json config files

--- dist/users/IUsersService.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IUsersService = void 0;
-;
-exports.IUsersService = Symbol.for("IUsersService");
-//# sourceMappingURL=IUsersService.js.map
